7184709a58c007f74668a687f0d82e5821a30c25
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-05-17 12:32:55 +0300

    copt

---
+++ PANCE/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 247, green: 251, blue: 255)
+    
+    static let mainColor = UIColor(integralRed: 112, green: 163, blue: 246)
+    static let mainColorAlpha = UIColor(integralRed: 112, green: 163, blue: 246, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 236, green: 90, blue: 101)
+    
+    static let progress1Color = UIColor(integralRed: 112, green: 163, blue: 246)
+    static let progress2Color = UIColor(integralRed: 236, green: 90, blue: 101)
+    static let progress3Color = UIColor(integralRed: 2, green: 46, blue: 106)
+}
