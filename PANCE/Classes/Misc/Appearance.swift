8a44419b5cdaba51d3cca53bcd70e2271eca1927
akonst17 <akonst17@gmail.com>
2022-08-02 09:44:29 +0300

    change appStoreUrl; build 7

--- PANCE/Classes/Misc/Appearance.swift
+++ PANCE/Classes/Misc/Appearance.swift
@@ -8,18 +8,14 @@
 import UIKit
 
 struct Appearance {
-    static let backgroundColor = UIColor(integralRed: 255, green: 252, blue: 254)
-    
     static let mainColor = UIColor(integralRed: 112, green: 163, blue: 246)
     static let mainColorAlpha = UIColor(integralRed: 112, green: 163, blue: 246, alpha: 0.3)
-    
-    static let takeFreeTestColor = UIColor(integralRed: 236, green: 90, blue: 101)
-    
-    static let progress1Color = UIColor(integralRed: 112, green: 163, blue: 246)
-    static let progress2Color = UIColor(integralRed: 236, green: 90, blue: 101)
-    static let progress3Color = UIColor(integralRed: 2, green: 46, blue: 106)
-    
+    static let secondColor = UIColor(integralRed: 236, green: 90, blue: 101)
+    static let thirdColor = UIColor(integralRed: 2, green: 46, blue: 106)
     static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
     static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
     static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
+    static let backgroundColor = UIColor(integralRed: 247, green: 251, blue: 255)
+    static let greyColor = UIColor(integralRed: 75, green: 81, blue: 102)
+    static let blackColor = UIColor(integralRed: 19, green: 24, blue: 42)
 }
--- PANCE/Classes/Misc/GlobalDefinitions.swift
+++ PANCE/Classes/Misc/GlobalDefinitions.swift
@@ -17,7 +17,8 @@ final class GlobalDefinitions {
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
 
-    static let appleAppID = "1568228652"
+    static let appStoreUrl = "https://apps.apple.com/app/pance-exam-prep-app/id1567717910"
+    static let appleAppID = "1567717910"
 
     static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
     static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
