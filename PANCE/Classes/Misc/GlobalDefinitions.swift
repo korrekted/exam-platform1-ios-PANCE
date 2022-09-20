cc67424aaa998458256ca125e82194448b839061
akonst17 <akonst17@gmail.com>
2022-09-20 11:56:57 +0300

    интеграция sdk 2.0; новые урлы; build 9

--- PANCE/Classes/Misc/GlobalDefinitions.swift
+++ PANCE/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "czSbAPhfUTETFaLx"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "2Adjb56rrTc5nDZ3"
 
     static let applicationTag = "EXAM_PANCE"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "2Adjb56rrTc5nDZ3"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "CpiOsQknOT4ibBkv"
 
     static let appStoreUrl = "https://apps.apple.com/app/pance-exam-prep-app/id1567717910"
     static let appleAppID = "1567717910"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
